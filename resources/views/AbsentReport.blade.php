32f07c554865a5ad16dd8acb9fb53bff6f09f470
William Galas <williamgalas2@gmail.com>
2021-12-05 23:36:00 +0800

    Update AbsentReport.blade.php

--- resources/views/AbsentReport.blade.php
+++ resources/views/AbsentReport.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Absent report
 
-Hello Mrs/Mr {{$parent-> name}}, Today ( {{now()->format('m-d-Y')}} ), Your Son/daughter {{$user->name}} is absent from class.
+Hello Mrs/Mr {{$parent-> name}}, Today ( {{now()->format('m-d-Y')}} ), Your Son/daughter {{$user->name}} is out of class.
 
 Thanks,<br>
 {{ config('app.name') }}
